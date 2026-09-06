09c45f2b1445006283495faa103e928f6d101044
siddu7575 <siddarthpadige7@gmail.com>
2026-09-06 11:58:18 +0530

    Add Assets

--- main.c
+++ main.c
@@ -3,11 +3,11 @@
 #include<util/delay.h>
 int main(void)
 {
-    GPIO_SetPin(GPIO_PORT_B, GPIO_PIN_5, GPIO_OUTPUT);
+  /*  GPIO_SetPin(GPIO_PORT_B, GPIO_PIN_5, GPIO_OUTPUT);
 
-    // GPIO_WritePin(GPIO_PORT_B, GPIO_PIN_5, GPIO_HIGH);  LED ON
+     GPIO_WritePin(GPIO_PORT_B, GPIO_PIN_5, GPIO_HIGH); // LED ON
     //GPIO_WritePin(GPIO_PORT_B, GPIO_PIN_5, GPIO_LOW);      // LED OFF
-
+*/
     UART_Init(9600);
     
 while(1){
@@ -15,5 +15,5 @@ UART_SendString("SIDDU\n");
 _delay_ms(1000);
 
 }
-
 }
+
